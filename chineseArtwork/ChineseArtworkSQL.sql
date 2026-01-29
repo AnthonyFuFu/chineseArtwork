a6f5a18bed54d6e7b3518ebef32e69ff23b299d0
Wits.AnthonyFu <Wits.AnthonyFu@wistron.com>
2026-01-29 17:52:14 +0800

    feat:add famous_artist  table
    add famous_artist  table

--- chineseArtwork/ChineseArtworkSQL.sql
+++ chineseArtwork/ChineseArtworkSQL.sql
@@ -22,8 +22,8 @@ CREATE TABLE [member] (
     MEM_BIRTHDAY DATE,                                     -- 生日
     MEM_PICTURE VARCHAR(300),                              -- 會員圖片路徑
     MEM_IMAGE VARCHAR(100),                                -- 會員圖片名稱
-    MEM_STATUS INT DEFAULT 1,                              -- 會員狀態(1:正常 0:停權)
-    MEM_VERIFICATION_STATUS INT DEFAULT 1,                 -- 會員驗證狀態(1:開通 0:停權)
+    MEM_STATUS INT NOT NULL DEFAULT 1,                     -- 會員狀態(1:正常 0:停權)
+    MEM_VERIFICATION_STATUS INT NOT NULL DEFAULT 0,        -- 會員驗證狀態(1:開通 0:停權)
     MEM_VERIFICATION_CODE VARCHAR(100),                    -- 會員驗證碼
     MEM_GOOGLE_UID VARCHAR(100),                           -- 會員googleUid
     CONSTRAINT UQ_MEMBER_ACCOUNT UNIQUE (MEM_ACCOUNT)
@@ -54,9 +54,9 @@ CREATE TABLE artist (
     ART_NAME NVARCHAR(20) NOT NULL,                        -- 姓名
     ART_PHONE VARCHAR(20) NOT NULL,                        -- 電話
     ART_GENDER CHAR(1),                                    -- 性別(f:女 m:男)
-    ART_GIVEN_NAME NVARCHAR(10) NOT NULL,                  -- 名
-    ART_COURTESY_NAME NVARCHAR(10) NOT NULL,               -- 字
-    ART_PSEUDONYM_NAME NVARCHAR(10) NOT NULL,              -- 號
+    ART_GIVEN_NAME NVARCHAR(10),                           -- 名
+    ART_COURTESY_NAME NVARCHAR(10),                        -- 字
+    ART_PSEUDONYM_NAME NVARCHAR(10),                       -- 號
     ART_BIRTHDAY DATE,                                     -- 生日
     ART_PICTURE VARCHAR(300),                              -- 照片路徑
     ART_IMAGE VARCHAR(100),                                -- 照片名稱
@@ -81,31 +81,66 @@ INSERT INTO artist (
     ART_EMAIL,
     ART_STATUS
 ) VALUES
-(N'傅勝宏', '0912345678', 'm', N'勝宏', N'勝子', N'宏者', '1994-09-17', 'sealcarving/admin/upload/images/carver/user.jpg', 'user.jpg', 'sealcarver101', 'carverpassword1', 's9017688@yahoo.com.tw', 1),
-(N'傅勝宏', '0912345678', 'm', N'勝宏', N'勝', N'者', '1994-09-17', 'sealcarving/admin/upload/images/carver/user.jpg', 'user.jpg', 'sealcarver102', 'carverpassword2', 's9017611@yahoo.com.tw', 1),
-(N'傅勝宏', '0912345678', 'f', N'勝宏', N'子', N'宏', '1994-09-17', 'sealcarving/admin/upload/images/carver/user.jpg', 'user.jpg', 'sealcarver103', 'carverpassword3', 's9017622@yahoo.com.tw', 1);
+(N'傅勝宏', '0912345678', 'm', N'勝宏', N'勝子', N'宏者', '1994-09-17', 'chineseArtwork/upload/images/artist/user.jpg', 'user.jpg', 'sealcarver101', 'carverpassword1', 's9017688@yahoo.com.tw', 1),
+(N'傅勝宏', '0912345678', 'm', N'勝宏', N'勝', N'者', '1994-09-17', 'chineseArtwork/upload/images/artist/user.jpg', 'user.jpg', 'sealcarver102', 'carverpassword2', 's9017611@yahoo.com.tw', 1),
+(N'傅勝宏', '0912345678', 'f', N'勝宏', N'子', N'宏', '1994-09-17', 'chineseArtwork/upload/images/artist/user.jpg', 'user.jpg', 'sealcarver103', 'carverpassword3', 's9017622@yahoo.com.tw', 1);
+-- 知名藝術家
+CREATE TABLE famous_artist (
+    FMS_ART_ID INT IDENTITY(1,1) NOT NULL PRIMARY KEY,     -- 知名藝術家ID
+    FMS_ART_NAME NVARCHAR(20) NOT NULL,                    -- 知名藝術家姓名
+    FMS_ART_GENDER CHAR(1),                                -- 性別(f:女 m:男)
+    FMS_ART_GIVEN_NAME NVARCHAR(10),                       -- 名
+    FMS_ART_COURTESY_NAME NVARCHAR(10),                    -- 字
+    FMS_ART_PSEUDONYM_NAME NVARCHAR(10),                   -- 號
+    FMS_ART_DESCRIPTION NVARCHAR(500),                     -- 知名藝術家介紹
+    FMS_ART_PICTURE VARCHAR(300),                          -- 照片路徑
+    FMS_ART_IMAGE VARCHAR(100),                            -- 照片名稱
+);
+INSERT INTO famous_artist (
+    FMS_ART_NAME, 
+    FMS_ART_GENDER, 
+    FMS_ART_GIVEN_NAME, 
+    FMS_ART_COURTESY_NAME, 
+    FMS_ART_PSEUDONYM_NAME, 
+    FMS_ART_DESCRIPTION, 
+    FMS_ART_PICTURE, 
+    FMS_ART_IMAGE
+) VALUES
+(N'王福庵','m',N'福庵',N'叔明',N'葉舟',N'王福庵（1883年－1965年），字叔明，號葉舟，浙江鄞縣人。他是中國近代著名的書法家、篆刻家，被譽為「近代篆書第一人」。','chineseArtwork/upload/images/famousArtist/wang_fu_an.jpg','wang_fu_an.jpg'),
+(N'齊白石','m',N'白石',N'文圭',N'籲吟',N'齊白石（1864年1月22日－1957年9月16日），原名齊純芝，後改名璜，一作璜之，改字白石，號濰翁、老木、公遺、木人、三百石印富翁、借山吟館主者等，湖南湘潭人，近現代中國繪畫、篆刻藝術大師，善於花鳥、山水和人物畫。','chineseArtwork/upload/images/famousArtist/qi_bai_shi.jpg','qi_bai_shi.jpg'
+);
 -- 作品分類
 CREATE TABLE category (
     CAT_ID INT IDENTITY(1,1) NOT NULL PRIMARY KEY,         -- 作品分類ID
     CAT_NAME NVARCHAR(30) NOT NULL,                        -- 作品分類名稱
     CAT_KEYWORD NVARCHAR(200) NOT NULL,                    -- 作品分類關鍵字
-    CAT_STATUS INT NOT NULL DEFAULT 1                      -- 狀態(0:下架 1:上架)
+    CAT_STATUS INT NOT NULL DEFAULT 1                      -- 狀態(0:不可選 1:可選)
 );
 INSERT INTO category (CAT_NAME, CAT_KEYWORD, CAT_STATUS) VALUES
-(N'人物肖像', N'肖像,人物,寫實,藝術', 1),
-(N'風景畫', N'山水,風景,自然,抽象', 1),
-(N'抽象創作', N'抽象,色彩,自由,創意', 1);
+(N'印章', N'篆刻,印章,書法,雕刻', 1),                        -- 把印章分類的關鍵字綁定
+(N'字畫', N'書法,行書,隸書,篆書', 1),                        -- 把字畫分類的關鍵字綁定
+(N'畫作', N'畫,繪畫,山水畫,花鳥畫,工筆畫,寫意', 1)            -- 把畫作分類的關鍵字綁定
+;
 -- 作品風格
 CREATE TABLE style (
     STYLE_ID INT IDENTITY(1,1) NOT NULL PRIMARY KEY,       -- 作品風格ID
     STYLE_NAME NVARCHAR(30) NOT NULL,                      -- 作品風格名稱
-    STYLE_KEYWORD NVARCHAR(200) NOT NULL,                  -- 作品風格關鍵字(抽象、寫實、寫意…)
+    STYLE_KEYWORD NVARCHAR(200) NOT NULL,                  -- 作品風格關鍵字(寫實、寫意)
     STYLE_STATUS INT NOT NULL DEFAULT 1                    -- 狀態(0:下架 1:上架)
 );
 INSERT INTO style (STYLE_NAME, STYLE_KEYWORD, STYLE_STATUS) VALUES
-(N'寫實', N'真實,精細,細膩,立體感', 1),
-(N'印象派', N'光影,色彩,筆觸,印象', 1),
-(N'水墨', N'水墨,傳統,中國畫,意境', 1);
+(N'寫實', N'寫實', 1),
+(N'寫意', N'寫意', 1),
+(N'花鳥畫', N'花鳥畫', 1),
+(N'工筆畫', N'工筆畫', 1),
+(N'走獸', N'走獸', 1),
+(N'鳥蟲', N'鳥蟲', 1),
+(N'山水', N'山水', 1),
+(N'篆書', N'篆書', 1),
+(N'隸書', N'隸書', 1),
+(N'楷書', N'楷書', 1),
+(N'草書', N'草書', 1),
+(N'行書', N'行書', 1);
 -- 作品
 CREATE TABLE artwork (
     AW_ID INT IDENTITY(1,1) NOT NULL PRIMARY KEY,          -- 作品ID
@@ -115,8 +150,8 @@ CREATE TABLE artwork (
     AW_TITLE NVARCHAR(100) NOT NULL,                       -- 作品名稱
     AW_DESCRIPTION NVARCHAR(500) NOT NULL,                 -- 作品說明 / 創作理念
     AW_PRICE DECIMAL(10,2) NOT NULL,                       -- 作品原價
-    AW_CREATED DATETIME2(3) NOT NULL,                      -- 創作年月
-    AW_DIMENSION NVARCHAR(50) NOT NULL,                    -- 尺寸(長×寬×高)
+    AW_CREATED DATETIME2(3),                               -- 創作年月
+    AW_DIMENSION NVARCHAR(100),                            -- 尺寸(長×寬×高)
     AW_IS_FOR_SALE INT NOT NULL DEFAULT 1,                 -- 販售(0:不販售 1:販售 2:售出)
     AW_STATUS INT NOT NULL DEFAULT 1,                      -- 展示(0:不展示 1:展示)
     AW_IS_DEL INT NOT NULL DEFAULT 0,                      -- 刪除(0:未刪除 1:刪除)
@@ -140,9 +175,8 @@ INSERT INTO artwork (
     AW_STATUS,
     AW_IS_DEL
 ) VALUES
-(1, 1, 1, N'晨曦', N'描繪清晨的光線與溫暖氛圍，表達新開始的希望', 15000.00, '2025-01-01', N'50×70×2 cm', 1, 1, 0),
-(2, 2, 2, N'夜之舞', N'以抽象手法表現夜晚城市的節奏與燈光律動', 22000.50, '2024-12-15', N'60×80×3 cm', 1, 1, 0),
-(3, 3, 3, N'山水情', N'傳統水墨風格，描繪山川與雲霧的意境', 18000.75, '2023-08-10', N'100×150×5 cm', 1, 1, 0);
+(1, 1, 1, N'吉祥', N'這是描述印章作品內容的', 15000.00, '2025-01-01', N'1.8×1.8 cm', 1, 1, 0),
+(2, 3, 1, N'夜之舞', N'以寫實手法表現夜晚城市', 22000.50, '2024-12-15', N'60×80×3 cm', 1, 1, 0);
 -- 作品圖片
 CREATE TABLE artwork_pic (
     AW_PIC_ID INT IDENTITY(1,1) NOT NULL PRIMARY KEY,      -- 作品圖片ID
@@ -154,17 +188,68 @@ CREATE TABLE artwork_pic (
 );
 INSERT INTO artwork_pic (AW_ID, AW_PIC_SORT, AW_PICTURE, AW_IMAGE)
 VALUES
-    (1, 1, 'chineseArtwork/admin/upload/images/artworkPic/20240731093732.jpg', '20240731093732.jpg'),
-    (1, 2, 'chineseArtwork/admin/upload/images/artworkPic/20240731093732.jpg', '20240731093732.jpg'),
-    (1, 3, 'chineseArtwork/admin/upload/images/artworkPic/20240731093732.jpg', '20240731093732.jpg'),
-    (2, 1, 'chineseArtwork/admin/upload/images/artworkPic/20240731093746.jpg', '20240731093746.jpg'),
-    (2, 3, 'chineseArtwork/admin/upload/images/artworkPic/20240731093746.jpg', '20240731093746.jpg'),
-    (2, 2, 'chineseArtwork/admin/upload/images/artworkPic/20240731093746.jpg', '20240731093746.jpg');
+(1, 1, 'chineseArtwork/upload/images/artworkPic/20240731093732.jpg', '20240731093732.jpg'),
+(1, 2, 'chineseArtwork/upload/images/artworkPic/20240731093732.jpg', '20240731093732.jpg'),
+(1, 3, 'chineseArtwork/upload/images/artworkPic/20240731093732.jpg', '20240731093732.jpg'),
+(2, 1, 'chineseArtwork/upload/images/artworkPic/20240731093746.jpg', '20240731093746.jpg'),
+(2, 3, 'chineseArtwork/upload/images/artworkPic/20240731093746.jpg', '20240731093746.jpg'),
+(2, 2, 'chineseArtwork/upload/images/artworkPic/20240731093746.jpg', '20240731093746.jpg');
+-- 知名藝術家作品
+CREATE TABLE famous_artwork (
+    FMS_AW_ID INT IDENTITY(1,1) NOT NULL PRIMARY KEY,      -- 知名藝術家作品ID
+    FMS_ART_ID INT NOT NULL,                               -- 知名藝術家ID
+    CAT_ID INT NOT NULL,                                   -- 分類ID
+    STYLE_ID INT NOT NULL,                                 -- 風格ID
+    FMS_AW_TITLE NVARCHAR(100) NOT NULL,                   -- 知名藝術家作品名稱
+    FMS_AW_DIMENSION NVARCHAR(100),                        -- 知名藝術家作品尺寸(長×寬×高)
+    FMS_AW_STATUS INT NOT NULL DEFAULT 1,                  -- 展示(0:不展示 1:展示)
+    FMS_AW_CREATE_TIME DATETIME2(3) DEFAULT SYSDATETIME(), -- 建立時間
+    FMS_AW_UPDATE_TIME DATETIME2(3) DEFAULT SYSDATETIME(), -- 修改時間
+    CONSTRAINT famous_artwork_famous_artist_fk FOREIGN KEY (FMS_ART_ID) REFERENCES famous_artist(FMS_ART_ID),
+    CONSTRAINT famous_artwork_category_fk FOREIGN KEY (CAT_ID) REFERENCES category(CAT_ID),
+    CONSTRAINT famous_artwork_style_fk FOREIGN KEY (STYLE_ID) REFERENCES style(STYLE_ID)
+);
+INSERT INTO famous_artwork (
+    FMS_ART_ID,
+    CAT_ID,
+    STYLE_ID,
+    FMS_AW_TITLE, 
+    FMS_AW_DIMENSION,
+    FMS_AW_STATUS
+)
+VALUES 
+(1, 1, 2, N'四君子篆刻作品', N'10cm x 10cm x 5cm', 1),       -- 作品 1：由王福庵創作，分類:印章，風格:寫意
+(1, 2, 11, N'嵩陽書院草書對聯', N'100cm x 30cm', 1),         -- 作品 2：由王福庵創作，分類:字畫，風格:寫意
+(2, 3, 4, N'螞蟻工筆畫', N'20cm x 20cm', 1),                -- 作品 3：由齊白石創作，分類:畫作，風格:寫意
+(2, 3, 2, N'荷花寫意畫', N'50cm x 70cm', 1);                -- 作品 4：由齊白石創作，分類:畫作，風格:寫意
+-- 知名藝術家作品圖片
+CREATE TABLE famous_artwork_pic (
+    FMS_AW_PIC_ID INT IDENTITY(1,1) NOT NULL PRIMARY KEY,  -- 知名藝術家作品圖片ID
+    FMS_AW_ID INT,                                         -- 知名藝術家作品ID
+    FMS_AW_PIC_SORT INT,                                   -- 知名藝術家作品圖片排序
+    FMS_AW_PICTURE VARCHAR(300),                           -- 知名藝術家作品圖片路徑
+    FMS_AW_IMAGE VARCHAR(100),                             -- 知名藝術家作品圖片名稱
+    CONSTRAINT famous_artwork_pic_famous_artwork_fk FOREIGN KEY (FMS_AW_ID) REFERENCES famous_artwork(FMS_AW_ID)
+);
+INSERT INTO famous_artwork_pic (FMS_AW_ID, FMS_AW_PIC_SORT, FMS_AW_PICTURE, FMS_AW_IMAGE)
+VALUES 
+-- 四君子篆刻作品 (ID: 1)
+(1, 1, 'chineseArtwork/upload/images/famousArtworkPic', 'four_gentlemen_carving_1.jpg'),
+(1, 2, 'chineseArtwork/upload/images/famousArtworkPic', 'four_gentlemen_carving_2.jpg'),
+-- 嵩陽書院草書對聯 (ID: 2)
+(2, 1, 'chineseArtwork/upload/images/famousArtworkPic', 'songyang_grass_script_couplet_1.jpg'),
+(2, 2, 'chineseArtwork/upload/images/famousArtworkPic', 'songyang_grass_script_couplet_2.jpg'),
+-- 螞蟻工筆畫 (ID: 3)
+(3, 1, 'chineseArtwork/upload/images/famousArtworkPic', 'ant_gongbi_1.jpg'),
+(3, 2, 'chineseArtwork/upload/images/famousArtworkPic', 'ant_gongbi_2.jpg'),
+-- 荷花寫意畫 (ID: 4)
+(4, 1, 'chineseArtwork/upload/images/famousArtworkPic', 'lotus_freehand_1.jpg'),
+(4, 2, 'chineseArtwork/upload/images/famousArtworkPic', 'lotus_freehand_2.jpg');
 -- 字體
 CREATE TABLE script_style (
     SCRIPT_ID INT IDENTITY(1,1) NOT NULL PRIMARY KEY,      -- 字體ID
     SCRIPT_WORD NVARCHAR(100) NOT NULL,                    -- 字體
-    SCRIPT_DESCRIPTION NVARCHAR(500) NOT NULL              -- 字體說明
+    SCRIPT_DESCRIPTION NVARCHAR(500)                       -- 字體說明
 );
 INSERT INTO script_style (SCRIPT_WORD, SCRIPT_DESCRIPTION)
 VALUES
@@ -202,7 +287,7 @@ CREATE TABLE dictionary (
     SCRIPT_ID INT NOT NULL,                                -- 字體ID
     RADICAL_ID INT NOT NULL,                               -- 部首ID
     DICT_WORD NVARCHAR(100) NOT NULL,                      -- 文字
-    DICT_DESCRIPTION NVARCHAR(500) NOT NULL,               -- 說文解字
+    DICT_DESCRIPTION NVARCHAR(500),                        -- 說文解字
     DICT_STROKES INT NOT NULL,                             -- 文字筆劃數量
     DICT_STATUS INT NOT NULL DEFAULT 1,                    -- 展示(0:不展示 1:展示)
     DICT_IS_DEL INT NOT NULL DEFAULT 0,                    -- 刪除(0:未刪除 1:刪除)
